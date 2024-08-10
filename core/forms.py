1337331126f69e5c1252282fef66949217d04702
Temba23 <tembasherpa80@gmail.com>
2024-08-10 21:35:26 +0545

    Authentication : Register Initial

--- core/forms.py
+++ core/forms.py
@@ -10,3 +10,23 @@ class LoginForm(forms.Form):
         label="Password",
         widget=forms.PasswordInput(attrs={'class': 'form-control'})
     )
+
+class RegisterForm(forms.Form):
+        username = forms.CharField(
+        label="Username",
+        max_length=30,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+        email = forms.CharField(
+        label="Email",
+        max_length=30,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+        password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
+        password2 = forms.CharField(
+        label="Confirm Password",
+        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+    )
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
-from .forms import LoginForm
+from .forms import LoginForm, RegisterForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
@@ -27,3 +27,27 @@ def logout(request):
 
 def home(request):
     pass
+
+def register(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            if User.objects.filter(email=email).exists():
+                return HttpResponse(f"User already exists with {email}. Please another one.")
+            else:
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password1']
+                password2 = form.cleaned_data['password2']
+                if password==password2:
+                    user = User.objects.create(username=username, email=email, password=password)
+                    user.save()
+                    return redirect('login')
+                else:
+                    return HttpResponse("Passwords dont match to each other.")
+        else:
+            return HttpResponse("Form Invalid.")
+    
+    else:
+        form = RegisterForm()
+        return render(request, "register.html", {"form":form})
