7dc70304d5c1ad9fac7d5bbb5143eb80b5498fac
Temba23 <tembasherpa80@gmail.com>
2024-08-17 12:29:33 +0545

    Alert : Initial Model

--- core/models.py
+++ core/models.py
@@ -1,4 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
+class Alert(models.Model):
+    user = models.ForeignKey(User, related_name="user_alert", on_delete=models.CASCADE)
+    scrip = models.CharField(max_length=15)
+    alert_on = models.IntegerField(max_length=6)
+    prev = models.IntegerField(max_length=6) 
+    today = models.IntegerField(max_length=6) 
