2e81d4254c50d822fd08e6a09126e2392241b9b7
Temba23 <tembasherpa80@gmail.com>
2024-08-19 20:53:54 +0545

    Notification : Email Setup

--- scrape/settings.py
+++ scrape/settings.py
@@ -13,6 +13,8 @@ import os
 
 from pathlib import Path
 
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,3 +136,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'your.smtp.server'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ['EMAIL']
+EMAIL_HOST_PASSWORD = os.environ['PASSWORD']
+DEFAULT_FROM_EMAIL = os.environ['EMAIL']
