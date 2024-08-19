5d246c655c8da16484b0e6522db2ae4d3c16b71e
Temba23 <tembasherpa80@gmail.com>
2024-08-19 20:45:59 +0545

    Notification : Initial idea for Notification in case of target hit

--- core/models.py
+++ core/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 import time
+
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your models here.
 
 class Alert(models.Model):
@@ -8,3 +11,14 @@ class Alert(models.Model):
     scrip = models.CharField(max_length=15)
     alert_on = models.IntegerField(max_length=6)
     today = models.IntegerField(max_length=6)
+
+    def check_and_send_alert(self):
+        if self.alert_on <= self.today:
+            self.send_alert()
+
+    def send_alert(self):
+        subject = f'Alert for {self.scrip}'
+        message = f'Dear {self.user.username},\n\nYour alert for {self.scrip} has been triggered.'
+        recipient_list = [self.user.email]
+        
+        send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, recipient_list)
